ed930c3f860a8e7e07762e60d7e33c130818c509
Gaurav Sen <gaurav@diverta.co.jp>
2022-06-07 16:52:34 +0900

    Fix hosting

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dc2105ed4d70a922"; }
+/******/ 		__webpack_require__.h = function() { return "9e81453645ff7dc4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c27c773764b72e31.hot-update.js
+++ .next/static/webpack/webpack.c27c773764b72e31.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "89ed52126fbae986"; }
+/******/ 	__webpack_require__.h = function() { return "9e81453645ff7dc4"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
