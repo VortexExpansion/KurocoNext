fd120240a36d44e7f3d418b8a826a7d1b7723e0f
Gaurav Sen <gaurav@diverta.co.jp>
2022-06-08 18:20:57 +0900

    Search query table

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "074f85bc903f78fa"; }
+/******/ 		__webpack_require__.h = function() { return "a28bdc52fc380918"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/sushi.js
+++ pages/sushi.js
@@ -1,8 +1,41 @@
-function sushi({posts}) {
+function sushi({posts,searchItems}) {
   console.log(posts.details)
+  console.log(searchItems.list)
   return (
     <>
     <div>About</div>
+    <br></br>
+
+    {/*---------- SearchItems ------------*/}
+    <div>
+    <table>
+  <thead>
+    <tr>
+      <th>Sushi</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+  <tbody>
+    {searchItems.list.map((searchItems)=>(
+      <>
+      <tr>
+        <td>{searchItems.ext_1}</td>
+        <td>{searchItems.ext_3}</td>
+      </tr>
+      </>
+    ))}
+  </tbody>
+  <tfoot>
+    <tr>
+      <td>Sum</td>
+      <td>$180</td>
+    </tr>
+  </tfoot>
+</table>
+    </div>
+    
+
+      {/*---------- Directory ---------- */}
     <br></br>
     <div>{posts.details.subject}</div>
     {/* <ul> */}
@@ -25,17 +58,26 @@ function sushi({posts}) {
     // Call an external API endpoint to get posts
     const res = await fetch(process.env.BASE_URL + '/rcms-api/3/service/4')
     const posts = await res.json()
+
+    const res2 = await fetch(process.env.BASE_URL + '/rcms-api/3/search')
+    const searchItems = await res2.json()
+    
+
+    //Logs for terminal
+
     console.log(res)
     console.log(posts.details)
     console.log(posts)
     console.log("")
+    console.log("Yaha se searchList")
+    console.log(searchItems)
     // By returning { props: { posts } }, the Blog component
     // will receive `posts` as a prop at build time
 
     
     return {
         props: {
-            posts,
+            posts, searchItems,
           },
     }
   }
