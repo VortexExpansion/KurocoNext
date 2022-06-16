be12a4c37b857d9b43d8fbcedf44c9e90c9de740
Gaurav Sen <gaurav@diverta.co.jp>
2022-06-17 01:39:46 +0900

    responsive animated text

--- components/animatedText.js
+++ components/animatedText.js
@@ -4,7 +4,7 @@ export default function AnimatedText({ text }) {
     return (
         <>
             <br></br>
-            <div className="text-8xl font-semibold text-center
+            <div className="md:text-8xl font-semibold text-center
             bg-gradient-to-r bg-clip-text  text-transparent 
             from-indigo-500 via-teal-500 to-violet-500
             animate-text
